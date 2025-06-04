836c155090cb11437cf59fd3fa2a4d9e59edf476
Alisa <github.reoccur462@passmail.net>
2025-06-04 11:00:35 -0400

    updates test to use gpa instead of page allocator

--- Zig_TOTP-generator/src/vault.zig
+++ Zig_TOTP-generator/src/vault.zig
@@ -1,21 +1,31 @@
 const std = @import("std");
 
+// Vault data structure should be a Hash map.
+// Each entry is name:secret
+// Json saved as name:secret as well
 pub const Vault = struct {
     accounts: []Account,
     parse: ?std.json.Parsed([]Account),
     buffer: ?[]u8,
 };
 
+ // Unneeded - each acc is an entry in the vault
+ // name and secret are already stored as key and value
 pub const Account = struct {
     name: []const u8,
     secret: []const u8,
 };
 
+ // Clean depends on the hash map structure
+ // No need for parse and buffer bs
 pub fn cleanVault(vault: *const Vault, allocator: std.mem.Allocator) void {
     vault.parse.?.deinit();
     allocator.free(vault.buffer.?);
 }
 
+ // Storing accs into the hash map will likely need to be done 1 by 1
+ // Can read file in 1 go, but still need to process accs 1 by 1
+ // Research!!
 pub fn readVault(dir: std.fs.Dir, filename: []const u8, allocator: std.mem.Allocator) !Vault {
 
     const json_file = try dir.openFile(filename, .{});
@@ -32,6 +42,7 @@ pub fn readVault(dir: std.fs.Dir, filename: []const u8, allocator: std.mem.Alloc
 
 }
 
+ // write name:secret, one per row ideally
 pub fn writeVault(dir: std.fs.Dir, filename: []const u8, allocator: std.mem.Allocator, accs: []Account) !void {
 
     var buffer = std.ArrayList(u8).init(allocator);
@@ -47,7 +58,9 @@ pub fn writeVault(dir: std.fs.Dir, filename: []const u8, allocator: std.mem.Allo
 
 test "write read test" {
 
-    const page_allocator = std.heap.page_allocator;
+    //const page_allocator = std.heap.page_allocator;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa_allocator = gpa.allocator();
     const expect = std.testing.expect;
     const eql = std.mem.eql;
 
@@ -61,10 +74,10 @@ test "write read test" {
     const accs = [_]Account{acc1, acc2, acc3, acc4};
     const ptrAccs = @constCast(accs[0..accs.len]);
 
-    try writeVault(dir, filename, page_allocator, ptrAccs);
+    try writeVault(dir, filename, gpa_allocator, ptrAccs);
 
-    const vault: Vault = try readVault(dir, filename, page_allocator);
-    defer cleanVault(&vault, page_allocator);
+    const vault: Vault = try readVault(dir, filename, gpa_allocator);
+    defer cleanVault(&vault, gpa_allocator);
 
     for (0..vault.accounts.len) |i| {
         try expect(eql(u8, accs[i].name, vault.accounts[i].name));
