1656ec1f2d57f210a0f6c960374a364e19af9bfc
Alisa <github.reoccur462@passmail.net>
2025-06-04 17:40:19 -0400

    Rewriting of vault.zig using hashmap instead of vault struct

--- Zig_TOTP-generator/src/vault.zig
+++ Zig_TOTP-generator/src/vault.zig
@@ -1,86 +1,105 @@
 const std = @import("std");
 
-// Vault data structure should be a Hash map.
-// Each entry is name:secret
-// Json saved as name:secret as well
-pub const Vault = struct {
-    accounts: []Account,
-    parse: ?std.json.Parsed([]Account),
-    buffer: ?[]u8,
-};
+pub fn readHMap(dir: std.fs.Dir, filename: []const u8, hmap: *std.StringHashMap([]const u8), allocator: std.mem.Allocator) !void {
 
- // Unneeded - each acc is an entry in the vault
- // name and secret are already stored as key and value
-pub const Account = struct {
-    name: []const u8,
-    secret: []const u8,
-};
-
- // Clean depends on the hash map structure
- // No need for parse and buffer bs
-pub fn cleanVault(vault: *const Vault, allocator: std.mem.Allocator) void {
-    vault.parse.?.deinit();
-    allocator.free(vault.buffer.?);
-}
-
- // Storing accs into the hash map will likely need to be done 1 by 1
- // Can read file in 1 go, but still need to process accs 1 by 1
- // Research!!
-pub fn readVault(dir: std.fs.Dir, filename: []const u8, allocator: std.mem.Allocator) !Vault {
-
+    // handle cases: filenotfound, accessdenied, notdir, invalidutf8, nametoolong, systemresources, os openerror
     const json_file = try dir.openFile(filename, .{});
     defer json_file.close();
 
+    // handle cases: outofmemory, endofstream, inputoutput, streamtoolong (file bigger than maxbytes), os readerror 
     const buffer = try json_file.readToEndAlloc(allocator, 10 * 1024); // 10 KB max
-    const parse = try std.json.parseFromSlice([]Account, allocator, buffer, .{});
+    // defer
+    // handle cases: outofmemory, unexpectedtoken, syntaxerror, duplicatefield, expectedvalue, expectedcomma, expectedcolon, invalidescape, invalidutf8
+    const parse = try std.json.parseFromSlice(std.json.Value, allocator, buffer, .{});
+    // defer
 
-    return Vault {
-        .accounts = parse.value,
-        .parse = parse,
-        .buffer = buffer,
-    };
+    const obj = parse.value;
+
+    // make copies of values to write into hashmap - must free buffer and parse memory
+    var iter = obj.object.iterator();
+    while (iter.next()) |entry| {
+        const key = entry.key_ptr.*;
+        const val = entry.value_ptr.*.string;
+
+        // handle outofmemory
+        try hmap.*.put(key, val);
+    }
+}
+
+
+test "read HMap from file" {
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    // handle access denied, notdir,pathalreadyexists,nametoolong,filenotfound,systemresources, os mkdirerror,openerror
+    const dir = try std.fs.cwd().makeOpenPath("test", .{});
+    const filename: []const u8 = "test.json";
+
+    var hmap = std.StringHashMap([]const u8).init(allocator);
+    try readHMap(dir, filename, &hmap, allocator);
+
+    var iter = hmap.keyIterator();
+    while (iter.next()) |key| {
+        const value = hmap.get(key.*).?;
+        std.debug.print("{s}:{s}\n", .{key.*, value});
+    } 
 
 }
 
- // write name:secret, one per row ideally
-pub fn writeVault(dir: std.fs.Dir, filename: []const u8, allocator: std.mem.Allocator, accs: []Account) !void {
+pub fn writeHMap(dir: std.fs.Dir, filename: []const u8, hmap: std.StringHashMap([]const u8), allocator: std.mem.Allocator) !void {
 
     var buffer = std.ArrayList(u8).init(allocator);
     defer buffer.deinit();
 
+    // handle accessdenied,pathalreadyexists,nametoolong,notdir,systemresources, os createrror
     var json_file = try dir.createFile(filename, .{});
     defer json_file.close();
 
-    try std.json.stringify(accs, .{}, buffer.writer());
-    try json_file.writeAll(buffer.items);
+    const writer = json_file.writer();
+    var json_stream = std.json.writeStream(writer, .{});
+    // handle writerror, streamtoolong
+    try json_stream.beginObject();
 
+    var it = hmap.iterator();
+    while (it.next()) |entry| {
+        try json_stream.objectField(entry.key_ptr.*);
+        try json_stream.write(entry.value_ptr.*);
+    }
+
+    // handle writerror,streamtoolong
+    try json_stream.endObject();
 }
 
-test "write read test" {
+test "write test" {
 
-    //const page_allocator = std.heap.page_allocator;
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const gpa_allocator = gpa.allocator();
-    const expect = std.testing.expect;
-    const eql = std.mem.eql;
+    const allocator = gpa.allocator();
 
     const dir = try std.fs.cwd().makeOpenPath("test", .{});
     const filename: []const u8 = "test.json";
 
-    const acc1 = Account { .name = "cuenta1", .secret = "CUENTA MIA" };
-    const acc2 = Account { .name = "account", .secret = "quack" };
-    const acc3 = Account { .name = "kurwa", .secret = "miku polish cow" };
-    const acc4 = Account { .name = "akhi", .secret = "falafel hummus pita" };
-    const accs = [_]Account{acc1, acc2, acc3, acc4};
-    const ptrAccs = @constCast(accs[0..accs.len]);
+    const acc1 = "cuenta1";
+    const acc2 = "cuenta2";
+    const acc3 = "cuenta3";
+    const sec1 = "CUENTA MIA";
+    const sec2 = "CUENTA NIA";
+    const sec3 = "CUENTA NYA";
 
-    try writeVault(dir, filename, gpa_allocator, ptrAccs);
+    var hmap = std.StringHashMap([]const u8).init(allocator);
 
-    const vault: Vault = try readVault(dir, filename, gpa_allocator);
-    defer cleanVault(&vault, gpa_allocator);
+    try hmap.put(acc1, sec1);
+    try hmap.put(acc2, sec2);
+    try hmap.put(acc3, sec3);
 
-    for (0..vault.accounts.len) |i| {
-        try expect(eql(u8, accs[i].name, vault.accounts[i].name));
-        try expect(eql(u8, accs[i].secret, vault.accounts[i].secret));
+    try writeHMap(dir, filename, hmap, allocator);
+
+    hmap.clearAndFree();
+    try readHMap(dir, filename, &hmap, allocator);
+
+    var iter = hmap.keyIterator();
+    while (iter.next()) |key| {
+        const value = hmap.get(key.*).?;
+        std.debug.print("{s}:{s}\n", .{key.*, value});
     }
 }
