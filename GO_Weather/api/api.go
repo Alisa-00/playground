87ff2e3c2e0d92d6342a2fe7b289fb55cb75dc71
Alisa-00 <github.reoccur462@passmail.net>
2025-08-06 15:56:58 +0300

    Adds forecast flag and functionality

--- GO_Weather/api/api.go
+++ GO_Weather/api/api.go
@@ -9,16 +9,25 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"time"
 )
 
-const baseUrl string = "http://api.openweathermap.org/data/2.5/weather"
+const baseUrl string = "http://api.openweathermap.org/data/2.5"
+const weatherEndpoint string = "weather"
+const forecastEndpoint string = "forecast"
+const dateFormat = time.Stamp
 
 type Weather struct {
 	City    string
 	Country string
-	Temp    float64
-	Feels   float64
-	Desc    string
+	List    []weatherDay
+}
+
+type weatherDay struct {
+	Temp  float64
+	Feels float64
+	Desc  string
+	Date  string
 }
 
 type Location struct {
@@ -28,7 +37,42 @@ type Location struct {
 	Longitude float64
 }
 
+type WeatherResponse struct {
+	Name string `json:"name"`
+	Main struct {
+		Temp  float64 `json:"temp"`
+		Feels float64 `json:"feels_like"`
+	} `json:"main"`
+	Weather []struct {
+		Main        string `json:"main"`
+		Description string `json:"description"`
+	} `json:"weather"`
+	Sys struct {
+		Country string `json:"country"`
+	} `json:"sys"`
+	Date int64 `json:"dt"`
+}
+
+type ForecastResponse struct {
+	List []struct {
+		Main struct {
+			Temp  float64 `json:"temp"`
+			Feels float64 `json:"feels_like"`
+		} `json:"main"`
+		Weather []struct {
+			Main        string `json:"main"`
+			Description string `json:"description"`
+		} `json:"weather"`
+		Date int64 `json:"dt"`
+	} `json:"list"`
+	City struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"city"`
+}
+
 func (loc Location) getQuerySubstring() string {
+
 	if loc.Latitude != 0 || loc.Longitude != 0 {
 		return fmt.Sprintf("lat=%f&lon=%f", loc.Latitude, loc.Longitude)
 	}
@@ -67,21 +111,6 @@ func CreateLocation(city string, country string, lat float64, lon float64) (Loca
 	return Location{}, fmt.Errorf("invalid input. city, country or latitude and longitude have to be valid")
 }
 
-type WeatherResponse struct {
-	Name string `json:"name"`
-	Main struct {
-		Temp  float64 `json:"temp"`
-		Feels float64 `json:"feels_like"`
-	} `json:"main"`
-	Weather []struct {
-		Main        string `json:"main"`
-		Description string `json:"description"`
-	} `json:"weather"`
-	Sys struct {
-		Country string `json:"country"`
-	} `json:"sys"`
-}
-
 func getConfigPath(filename string) string {
 	_, b, _, _ := runtime.Caller(0)
 	basepath := filepath.Dir(b)
@@ -100,13 +129,13 @@ func getApiKey() (string, error) {
 
 func GetWeather(loc Location, units string) (Weather, error) {
 
-	const url string = baseUrl
 	apiKey, err := getApiKey()
 	if err != nil {
 		return Weather{}, err
 	}
+
 	locationParams := loc.getQuerySubstring()
-	query := fmt.Sprintf("%s?%s&appid=%s&units=%s", url, locationParams, apiKey, units)
+	query := fmt.Sprintf("%s/%s?%s&appid=%s&units=%s", baseUrl, weatherEndpoint, locationParams, apiKey, units)
 
 	resp, err := http.Get(query)
 	if err != nil {
@@ -133,10 +162,67 @@ func GetWeather(loc Location, units string) (Weather, error) {
 	}
 
 	return Weather{
-		data.Name,
-		data.Sys.Country,
-		data.Main.Temp,
-		data.Main.Feels,
-		data.Weather[0].Description,
+		City:    data.Name,
+		Country: data.Sys.Country,
+		List: []weatherDay{
+			{
+				Temp:  data.Main.Temp,
+				Feels: data.Main.Feels,
+				Desc:  data.Weather[0].Description,
+				Date:  time.Unix(data.Date, 0).Format(dateFormat),
+			},
+		},
+	}, nil
+}
+
+func GetForecast(loc Location, units string) (Weather, error) {
+
+	apiKey, err := getApiKey()
+	if err != nil {
+		return Weather{}, err
+	}
+
+	locationParams := loc.getQuerySubstring()
+	query := fmt.Sprintf("%s/%s?%s&appid=%s&units=%s", baseUrl, forecastEndpoint, locationParams, apiKey, units)
+
+	resp, err := http.Get(query)
+	if err != nil {
+		return Weather{}, fmt.Errorf("request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return Weather{}, fmt.Errorf("bad status code: %s (status code %d)", resp.Status, resp.StatusCode)
+	}
+
+	var data ForecastResponse
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return Weather{}, fmt.Errorf("error reading response: %w", err)
+	}
+
+	if err := json.Unmarshal(body, &data); err != nil {
+		return Weather{}, fmt.Errorf("invalid JSON: %w", err)
+	}
+
+	if len(data.List) == 0 {
+		return Weather{}, fmt.Errorf("no forecast data found")
+	}
+
+	weatherList := make([]weatherDay, 0)
+	for _, data := range data.List {
+		day := weatherDay{
+			Temp:  data.Main.Temp,
+			Feels: data.Main.Feels,
+			Desc:  data.Weather[0].Description,
+			Date:  time.Unix(data.Date, 0).Format(dateFormat),
+		}
+		weatherList = append(weatherList, day)
+	}
+
+	return Weather{
+		City:    data.City.Name,
+		Country: data.City.Country,
+		List:    weatherList,
 	}, nil
 }
--- GO_Weather/weather.go
+++ GO_Weather/weather.go
@@ -12,43 +12,62 @@ func main() {
 	// read flags
 	var city, country, units string
 	var lat, lon float64
+	var forecast bool
 	flag.StringVar(&city, "city", "", "City name")
 	flag.StringVar(&city, "c", "", "Shorthand for -city")
 	flag.StringVar(&country, "country", "", "Country code")
 	flag.StringVar(&country, "C", "", "Shorthand for country code")
 	flag.StringVar(&units, "units", "metric", "Units for displaying")
 	flag.StringVar(&units, "u", "metric", "Shorthand for units")
+	flag.BoolVar(&forecast, "f", false, "Forecast")
 	flag.Float64Var(&lat, "lat", 0, "Latitude")
 	flag.Float64Var(&lon, "lon", 0, "Longitude")
 	flag.Parse()
 
+	// parse and validate inputs
 	units, err := parse.ParseUnits(units)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	char, err := parse.GetChar(units)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	location, err := api.CreateLocation(city, country, lat, lon)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// get weather
-	weather, err := api.GetWeather(location, units)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if forecast {
+		weather, err := api.GetForecast(location, units)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		display(weather, char)
+	} else {
+		weather, err := api.GetWeather(location, units)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		display(weather, char)
 	}
 
 	// print on screen
-	char, err := parse.GetChar(units)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	display(weather, char)
+	//display(weather, char)
 }
 
 func display(weather api.Weather, char rune) {
-	fmt.Printf("%s,%s: %.1f°%c, %s. It feels like %.1f°%c\n", weather.City, weather.Country, weather.Temp, char, weather.Desc, weather.Feels, char)
+	fmt.Printf("%s,%s\n", weather.City, weather.Country)
+	for _, day := range weather.List {
+		fmt.Printf("%s: %.1f°%c, %s. It feels like %.1f°%c\n", day.Date, day.Temp, char, day.Desc, day.Feels, char)
+	}
+
+	//fmt.Printf("%s,%s: %.1f°%c, %s. It feels like %.1f°%c\n", weather.City, weather.Country, weather.Temp, char, weather.Desc, weather.Feels, char)
 }
